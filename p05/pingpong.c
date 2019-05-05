c83dd895e6d45561d8c4624ae2bed723435f4f5a
Rafael Garcia <rafaelgarcia95@gmail.com>
2019-05-05 20:36:43 -0300

    Implemeting preemption on system task management

--- p05/pingpong.c
+++ p05/pingpong.c
@@ -3,13 +3,18 @@
 
 #include "pingpong.h"
 #include "queue.h"
+#include <signal.h>
+#include <sys/time.h>
 
 #define STACK_SIZE 30000
 
 #define TYPE_SYSTEM_TASK 's'
 #define TYPE_KERNEL_TASK 'k'
 
+#define QUANTUM_SIZE_IN_TICKS 20
+
 int task_identifier = 0;
+int remaining_ticks = QUANTUM_SIZE_IN_TICKS;
 
 task_t main_task;
 task_t *current_task;
@@ -18,55 +23,105 @@ task_t dispatcher_task;
 queue_t *ready_queue;
 queue_t *suspended_queue;
 
+struct sigaction action;
+struct itimerval timer;
+
 //================================================================================
 // Debug functions
 //================================================================================
 
 void printBootMessage() {
     #ifdef DEBUG
-        printf ("Booting operational system - Starting main task with TID: %d \n", task_identifier);
+        printf ("Booting operational system - Starting main task with TID: %d ", task_identifier);
     #endif
 }
 
 void printTaskCreatedMessage(int task_tid) {
     #ifdef DEBUG
-        printf ("Successfully created new task with TID: %d \n", task_tid);
+        printf ("Successfully created new task with TID: %d ", task_tid);
     #endif
 }
 
 void printTaskExitingMessage(int current_task_tid) {
     #ifdef DEBUG
-        printf ("Gracefully exiting current task with TID: %d \n", current_task_tid);
+        printf ("Gracefully exiting current task with TID: %d ", current_task_tid);
     #endif
 }
 
 void printTryingToSwapContextsMessage(int task_to_be_swapped_tid, int current_task_tid) {
     #ifdef DEBUG
-        printf ("Trying to swap context from task with TID: %d to task with TID with TID: %d \n", task_to_be_swapped_tid, current_task_tid);
+        printf ("Trying to swap context from task with TID: %d to task with TID with TID: %d ", task_to_be_swapped_tid, current_task_tid);
     #endif
 }
 
 void printContextSwappedMessage() {
     #ifdef DEBUG
-        printf ("Successfully swapped contexts! \n");
+        printf ("Successfully swapped contexts! ");
     #endif
 }
 
 void printSettingPriorityOutsideOfAcceptableBoundsMessage() {
     #ifdef DEBUG
-        printf("Trying to set task priority outside of acceptable UNIX bounds, setting to default instead. \n");
+        printf("Trying to set task priority outside of acceptable UNIX bounds, setting to default instead.");
     #endif
 }
 
 void printDefinedTaskTypeMessage(task_t* task) {
-    #ifdef DEBUG_NOW
+    #ifdef DEBUG
         printf("Task with TID: %d type has been defined to: %c \n", task->tid, task->task_type);
+    #endif
+}
+
+void printEndOfQuantumMessage() {
+    #ifdef DEBUG
+        printf("End of task with TID: %d quantum \n", current_task->tid);
     #endif
 }
 
 //================================================================================
 // Auxiliary functions
 //================================================================================
+
+int isPriorityBetweenAcceptableBounds(int prio) {
+    if (prio >= -20 && prio <= 20) {
+        return 1;
+    }
+    printSettingPriorityOutsideOfAcceptableBoundsMessage();
+    return 0;
+}
+
+void defineTaskType(task_t* task) {
+    if (task == &dispatcher_task) {
+        task->task_type = TYPE_KERNEL_TASK;
+    } else {
+        task->task_type = TYPE_SYSTEM_TASK;
+    }
+    printDefinedTaskTypeMessage(task);
+}
+
+//===================================================================================
+// Preemption related functions
+//===================================================================================
+
+void resetRemainingTicksQuantity() {
+    remaining_ticks = QUANTUM_SIZE_IN_TICKS;
+}
+
+void systemClockTickHandler() {
+    if(current_task->task_type != TYPE_KERNEL_TASK) {
+        remaining_ticks--;
+
+        if(remaining_ticks == 0) {
+            printEndOfQuantumMessage();
+            resetRemainingTicksQuantity();
+            task_yield();
+        }
+    }
+}
+
+//===================================================================================
+// Scheduler function
+//===================================================================================
 
 task_t* scheduler() {
     queue_t* ready_queue_iterator;
@@ -109,6 +164,10 @@ task_t* scheduler() {
     return NULL;
 }
 
+//===================================================================================
+// Dispatcher function
+//===================================================================================
+
 void dispacherBody(void *arg) {
     while (queue_size(ready_queue) > 0) {
 
@@ -122,26 +181,9 @@ void dispacherBody(void *arg) {
     task_exit(0);
 }
 
-int isPriorityBetweenAcceptableBounds(int prio) {
-    if (prio >= -20 && prio <= 20) {
-        return 1;
-    }
-    printSettingPriorityOutsideOfAcceptableBoundsMessage();
-    return 0;
-}
-
-void define_task_type(task_t* task) {
-    if (task == &dispatcher_task) {
-        task->task_type = TYPE_KERNEL_TASK;
-    } else {
-        task->task_type = TYPE_SYSTEM_TASK;
-    }
-
-    printDefinedTaskTypeMessage(task);
-}
-
-
-/*===================================================================================*/
+//===================================================================================
+// Project functions
+//===================================================================================
 
 void pingpong_init() {
     printBootMessage();
@@ -151,8 +193,27 @@ void pingpong_init() {
     main_task.task_type = TYPE_SYSTEM_TASK;
     current_task = &main_task;
 
+    //
+    action.sa_handler = systemClockTickHandler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    if(sigaction(SIGALRM, &action, 0) < 0) {
+        perror("Sigaction error: ");
+        exit(1);
+    }
+    timer.it_value.tv_usec = 1000;
+    timer.it_value.tv_sec = 0;
+    timer.it_interval.tv_usec = 1000;
+    timer.it_interval.tv_sec = 0;
+    if(setitimer(ITIMER_REAL, &timer, 0) < 0) {
+        perror("Setitimer error: ");
+        exit(1);
+    }
+
     // Creating dispatcher task
     task_create(&dispatcher_task, &dispacherBody, NULL);
+
+
 
     setvbuf(stdout, 0, _IONBF, 0);
 }
@@ -183,8 +244,7 @@ int task_create(task_t *task, void (*start_func)(void *), void *arg) {
 
     task->static_prio = 0;
     task->dynamic_prio = 0;
-
-    define_task_type(task);
+    defineTaskType(task);
 
     printTaskCreatedMessage(task->tid);
 
@@ -210,9 +270,7 @@ int task_switch (task_t *task) {
     current_task = task;
 
     printTryingToSwapContextsMessage(task_to_be_swapped->tid, current_task->tid);
-
     swapcontext(&task_to_be_swapped->context, &current_task->context);
-
     printContextSwappedMessage();
 
     return 0;
